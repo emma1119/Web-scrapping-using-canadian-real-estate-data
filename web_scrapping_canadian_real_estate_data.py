ba741a7a46b0df91a88a1cff280b196e56886ea5
Emma <hongfan.liu1119@gmail.com>
2023-02-26 21:26:53 -0500

    update python code
    step 1: write a function to scrap the web page using url and selenium
    step 2: parse the data from html and save the interested elements into csv

--- web_scrapping_canadian_real_estate_data.py
+++ web_scrapping_canadian_real_estate_data.py
@@ -2,43 +2,56 @@
 # Date: 2023/2/23 22:28
 # Script name:  Web_scapping
 
-# import inline as inline
 from lib2to3.pgen2 import driver
-
 from bs4 import BeautifulSoup  # For HTML parsing
 from time import sleep         # To prevent overwhelming the server between connections
 from requests import get
 from selenium import webdriver # API that allows you to programmatically interact with a browser the way a real user would.
 from selenium.webdriver.common.keys import Keys
-# driver.get("https://condos.ca")
 import pandas as pd
 import os
 import time
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-#
+import selenium
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 # os – for accessing our folder structures
 # time – for putting sleep functionality which will pause our code for a few seconds
 # selenium – for automating website navigation flow and downloading HTML content
 # bs4 – for extracting relevant data from HTML page source
-from selenium import webdriver
 
+
+# print(selenium.__version__)
+
+# create an instance of the webdriver class, which represents a browser window.
+# launching chrome wedriver
 driver = webdriver.Chrome()
 url = 'https://www.realtor.ca/on/toronto/real-estate-for-sale'
 # url = "https://condos.ca"
-driver.get(url)
+## Smaller list- 155: 600K to 1.2Million RH listing
+url2= 'https://www.realtor.ca/map#LatitudeMax=43.97778&LongitudeMax=-79.37101&LatitudeMin=43.82935&LongitudeMin=-79.48559&view=list&Sort=6-D&PGeoIds=g30_dpz9b86g&PropertyTypeGroupID=1&PropertySearchTypeId=1&TransactionTypeId=2&PriceMin=600000&PriceMax=1200000&Currency=CAD'
+# call the get method on that instance to navigate to a website.
+# driver.get(url2)
 
 
+
+## Step 1: function to scrap the web page
 def scrapping_function(current_url, click_next_button):
+    # driver = webdriver.Chrome()
     driver.get(current_url)
-    i = 1
+    i = 0
     next_page_true = True
     while next_page_true:
-        page = driver.page_source
-        file_ = open('saved_pages/toronto/page_{}.html'.format(i), 'w')
+        page = driver.page_source # driver.page_source is saving all the HTML content of the page in the variable which is saved as an HTML file in the local folder.
+        a=i+1
+        file_ = open(r'C:\\Users\PC\Documents\GitHub\Web-scrapping-using-canadian-real-estate-data\file\page_{}.html'.format(a), 'w', encoding='utf-8')
         file_.write(page)
         file_.close()
         page_url = driver.current_url
-        elem = driver.find_element_by_xpath(click_next_button)
+        wait = WebDriverWait(driver, 600)
+        elem = wait.until(EC.visibility_of_element_located((By.XPATH, click_next_button)))
         elem.click()
         time.sleep(2)
         page_url_next = driver.current_url
@@ -46,15 +59,46 @@ def scrapping_function(current_url, click_next_button):
             next_page_true = False
         else:
             i += 1
+# define the XPath of the "Next" button
+click_next_button ='/html/body/form/div[5]/div[2]/span/div/div[4]/div[3]/span/span/div/a[3]' #'//*[@id="ListViewPagination_Bottom"]/div/a[3]'
 
-# initializing needed variables
-
-click_next_button = "/html/body/form/div[5]/div[2]/span/div/div[4]/div[3]/span/span/div/a[3]"
-
-# launching firefox wedriver
-# binary = FirefoxBinary('Location_for _firefox_installation')
-# driver = webdriver.Firefox(firefox_binary=binary)
-#
 # # calling scrapping function
-# scrapping_function(realtor_url, click_next_button)
-# driver.close()
+scrapping_function(url2, click_next_button)
+driver.close()
+
+
+##Step 2: getting data from each pages
+df = pd.DataFrame()
+
+for file in os.listdir('C:/Users/PC/Documents/GitHub/Web-scrapping-using-canadian-real-estate-data/file'):
+    html_file = r'C:/Users/PC/Documents/GitHub/Web-scrapping-using-canadian-real-estate-data/file/{}'.format(file)
+    soup = BeautifulSoup(open(html_file, encoding='utf-8'), "html.parser")
+    data = soup.find_all(class_="cardCon")
+    for i, elem in enumerate(data):
+        price = data[i].find(class_="listingCardPrice").get_text()
+        address = data[i].find(class_="listingCardAddress").get_text()
+        rooms = data[i].find_all(class_="listingCardIconNum")
+        bedrooms = rooms[0].get_text()
+        bathrooms = rooms[1].get_text()
+        try:
+            row_df = pd.DataFrame({'price': [price],
+                                   'address': [address],
+                                   'bedrooms': [bedrooms],
+                                   'bathrooms': [bathrooms],
+                                   'page': [file]})
+            df = pd.concat([df, row_df])
+            del row_df
+        except:
+            print("Error in record_{}".format(i))
+
+
+# cleaning dataframe
+def cleaning_address(x):
+    return (x.replace("\n", "").strip())
+
+
+df['address'] = df['address'].apply(cleaning_address)
+
+# saving data file
+df.to_csv(r'C:/Users/PC/Documents/GitHub/Web-scrapping-using-canadian-real-estate-data/output/toronto.csv'\
+          , index=False)
